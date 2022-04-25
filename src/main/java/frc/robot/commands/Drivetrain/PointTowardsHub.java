e8005b05547e8b24095de5b5355156948a8c7a7b
SteptimusHeap <31784207+SteptimusHeap@users.noreply.github.com>
2022-04-25 19:53:00 -0400

    stuff

---
+++ src/main/java/frc/robot/commands/Drivetrain/PointTowardsHub.java
@@ -0,0 +1,33 @@
+package frc.robot.commands.Drivetrain;
+
+import frc.robot.subsystems.SwerveDriveSubsystem;
+
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+
+public class PointTowardsHub extends CommandBase {
+    private final SwerveDriveSubsystem drivetrain;
+    public PIDController LimeLightRotationPID;
+
+
+    public PointTowardsHub(SwerveDriveSubsystem drivetrain) {
+        this.drivetrain = drivetrain;
+    }
+    @Override
+    public void initialize() {
+
+    }
+
+    @Override
+    public void execute() {
+        drivetrain.CommandVariable = "TowardsHub";
+        drivetrain.pointTowardsHub(true);
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+        // Stop the drivetrain
+        drivetrain.setSpeeds(new ChassisSpeeds(0.0, 0.0, 0.0));
+    }
+}
